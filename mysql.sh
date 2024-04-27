d6fdefb97fe8ed0a68ff460c9f3a0b139c6b645e
Viswa_addanki <a.viswanath007@gmail.com>
2024-04-27 21:53:46 +0530

    some-msg

--- mysql.sh
+++ mysql.sh
@@ -36,5 +36,15 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting root password"
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting root password"
+
+# below line ia usefull for idempotent purpos
+mysql -h db.viswaws.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
