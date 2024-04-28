b1e88ad3276d1f9ad595a423be4d6fa3b8b16515
Viswa_addanki <a.viswanath007@gmail.com>
2024-04-28 11:03:29 +0530

    some-msg

--- Backend.sh
+++ Backend.sh
@@ -10,6 +10,7 @@ Y="\e[33m"
 N="\e[0m"
 echo "Please enter DB password:"
 read -s mysql_root_password
+
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
@@ -29,65 +30,55 @@ else
 fi
 
 dnf module disable nodejs -y &>>$LOGFILE
- VALIDATE $? "Diabilong default nodesjs"
+VALIDATE $? "Disabling default nodejs"
 
- dnf module enable nodejs:20 -y &>>$LOGFILE
- VALIDATE $? "Enabling  nodejs 20"
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
 
- dnf install nodejs -y &>>$LOGFILE
- VALIDATE $? "instaling  nodejs 20"
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
 
- id expense &>>$LOGFILE
-
- if [ $? -ne 0 ]
- then
-     useradd expense
-     VALIDATE $? "Creating a new user"
-
-     else
-      echo -e "User is already created...$Y SKIPPING $N"
-fi
-
-  mkdir -p /app &>>$LOGFILE
-     VALIDATE $? "Creating  app directory" 
-
-     curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
-  VALIDATE $? "Downloading Backend code" 
-
-  cd /app
-  rm -rf /app/*
-  unzip /tmp/backend.zip &>>$LOGFILE
-
-  VALIDATE $? "Extracted backend code"
-
-  npm install
-VALIDATE $? "Isntalling nodejs dependencies"  
- 
- cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
-  VALIDATE $? "Copied backend service"
-
-  systemctl daemon-reload &>>$LOGFILE
- VALIDATE $? "Daemon reload"
-
-  systemctl start backend &>>$LOGFILE
-VALIDATE $? "Starting backend"
-
- systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
-
- ddnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Client"
-
-mysql -h db.viswaws.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
 else
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+    echo -e "Expense user already created...$Y SKIPPING $N"
 fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading backend code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
+
+#check your repo and path
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h db.viswaws.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
 systemctl restart backend &>>$LOGFILE
 VALIDATE $? "Restarting Backend"
-
-
- 
